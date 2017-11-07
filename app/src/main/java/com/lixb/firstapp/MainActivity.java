8cad0c298e0767bac616afa7170aa71972e8bf59
lixiaobin <928060035@qq.com>
2017-11-07 16:22:14 +0800

    修改文件

--- app/src/main/java/com/lixb/firstapp/MainActivity.java
+++ app/src/main/java/com/lixb/firstapp/MainActivity.java
@@ -3,15 +3,20 @@ package com.lixb.firstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int x = 1;
+
+        Log.d(TAG, "onCreate: haha");
     }
 
     public void toLoginActiv(View view) {
